b4fa40b59e923da34cbc8f520364d08f954c37be
Muhammad Farooq <m-farooq1@hotmail.com>
2016-05-17 21:08:44 +0500

    initial

---
+++ KnowSys-Cordova-App/www/js/controllers/purchasesCtrl.js
@@ -0,0 +1,27 @@
+﻿angular.module('knowSysApp').controller("purchasesCtrl", ["$scope", "$ionicLoading",
+            function ($scope, $ionicLoading) {
+    $scope.creditPackItems = [];
+    $scope.creditItems = [];
+    $scope.currencySymbol = "$";
+    $scope.init = function () {
+        for (var i = 0; i < 2; i++) {
+            $scope.creditItems = $scope.creditItems.concat(
+            {
+                productId: i + 1,
+                name: "Credit Item " + (i + 1),
+                price: (i+1) * 10
+            });
+            $scope.creditPackItems = $scope.creditPackItems.concat(
+            {
+                productId: i+1,
+                name: "Credit Pack " + (i + 1),
+                price: (i + 2) * 10
+            });
+        }
+    };
+    
+    $scope.buyItem = function () {
+        $ionicLoading.show();
+        setTimeout(function () { $ionicLoading.hide() }, 500);
+    };
+}]);
